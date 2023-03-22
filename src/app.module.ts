305647b15a8ed190952592f6a8339621228ab873
Breno Felix <breno.lessa@hashsys.com.br>
2023-03-22 19:43:02 -0300

    :construction: create challenges module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PlayersModule } from './players/players.module';
 import { DatabaseModule } from './database/database.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ChallengesModule } from './challenges/challenges.module';
 
 @Module({
-  imports: [PlayersModule, DatabaseModule, CategoriesModule],
+  imports: [PlayersModule, DatabaseModule, CategoriesModule, ChallengesModule],
   controllers: [],
   providers: [],
 })
